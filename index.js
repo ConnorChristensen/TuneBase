5c145ca6e676d00eee56da52692b4ea77144e17e
Connor Christensen <chriconn@oregonstate.edu>
2018-04-10 19:30:47 -0700

    created an entry html and js file

---
+++ index.js
@@ -0,0 +1,44 @@
+// import electron into the app and BrowserWindow variables
+const {app, BrowserWindow} = require('electron')
+
+const path = require('path')
+const url = require('url')
+
+function createWindow() {
+  // create the browser window
+  win = new BrowserWindow({width: 800, height: 600})
+
+  // load the index.html of the app
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file',
+    slashes: true
+  }))
+
+  // open up the development tools
+  win.webContents.openDevTools()
+
+  win.on('closed', () => {
+    // dereference our closed window, we don't need it
+    win = null
+  })
+}
+
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  // if you are not in mac, quit the app when closing
+  // all the windows
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // if you are on a mac, open a window when you click
+  // on the app if there aren't already open windows
+  if (win === null) {
+    createWindow()
+  }
+})
